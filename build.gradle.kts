ad909b8cfeb8438d8f9b9b0840618989329e4688
Sergey Titov <ya.sergeTitov@yandex.ru>
2024-10-31 15:02:50 +0300

    Update greeting in App.java and configure Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
+    application
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
 
 repositories {
     mavenCentral()
